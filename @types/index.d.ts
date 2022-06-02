c55f7aecc13e707970ecbf0eead592652ef46a7d
Lucas ARRIZABALAGA <arrizabalucas@gmail.com>
2022-06-02 18:21:31 +0200

    modifying index file

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,10 +1,11 @@
+import IPartner from '../interfaces/IPartner';
 import IUser from '../interfaces/IUser';
 import IUserInfo from '../interfaces/IUserInfoInit';
 declare global {
   namespace Express {
     interface Request {
       userInfo?: IUserInfo;
-      record?: IUser; // used to store deleted record to send appropriate responses to react-admin
+      record?: IUser | IPartner; // used to store deleted record to send appropriate responses to react-admin
     }
   }
 }
