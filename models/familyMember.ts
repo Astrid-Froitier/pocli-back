ef8749ff3a375ac56e25910107f92a9bf4233d13
Astrid-Froitier <asfroitier@gmail.com>
2022-06-16 10:03:07 +0200

    added familyMember model

---
+++ models/familyMember.ts
@@ -0,0 +1,74 @@
+// import { Request, Response, NextFunction } from 'express';
+import { ResultSetHeader } from 'mysql2';
+import connection from '../db-config';
+//import { ErrorHandler } from '../helpers/errors';
+import IFamilyMember from '../interfaces/IFamilyMember';
+
+// to see if the email is available
+// const emailIsFree = async (req: Request, res: Response, next: NextFunction) => {
+//   const familyMember = req.body as IFamilyMember;
+//   const familyMemberExists: IFamilyMember = await getFamilyMemberByEmail(
+//     familyMember.email
+//   );
+//   if (familyMemberExists) {
+//     next(new ErrorHandler(409, `This email's member already exists`));
+//   } else {
+//     next();
+//   }
+// };
+
+const getAllfamilyMembers = async (sortBy = ''): Promise<IFamilyMember[]> => {
+  let sql =
+    'SELECT id, idFamily, firstname, birthday, isActive FROM familyMembers';
+  if (sortBy) {
+    sql += `ORDER BY ${sortBy}`;
+  }
+  const results = await connection.promise().query<IFamilyMember[]>(sql);
+  return results[0];
+};
+
+const getFamilyMemberById = async (
+  idFamily: number
+): Promise<IFamilyMember> => {
+  const [results] = await connection
+    .promise()
+    .query<IFamilyMember[]>(
+      'SELECT id, firstname, birthday FROM familyMembers WHERE id =  ?',
+      [idFamily]
+    );
+  return results[0];
+};
+
+const addFamilyMember = async (
+  familyMember: IFamilyMember
+): Promise<number> => {
+  //   const hashedPassword = await hashPassword(familyMember.password);
+  const results = await connection
+    .promise()
+    .query<ResultSetHeader>(
+      'INSERT INTO familyMembers (idFamily, firstname, birthday, isActive) VALUES (?, ?, ?, ?)',
+      [
+        familyMember.idFamily,
+        familyMember.firstname,
+        familyMember.birthday,
+        familyMember.isActive,
+      ]
+    );
+  return results[0].insertId;
+};
+
+const deleteFamilyMember = async (idFamilyMember: number): Promise<boolean> => {
+  const results = await connection
+    .promise()
+    .query<ResultSetHeader>('DELETE FROM familyMembers WHERE id = ?', [
+      idFamilyMember,
+    ]);
+  return results[0].affectedRows === 1;
+};
+
+export default {
+  getAllfamilyMembers,
+  getFamilyMemberById,
+  addFamilyMember,
+  deleteFamilyMember,
+};
