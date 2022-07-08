d9ff675e3cfb03d6c8fc6a118aae564ea2d59e58
fblaizoud <blaiz95@hotmail.com>
2022-07-08 09:39:59 +0200

    .env update for heroku

--- db-config.ts
+++ db-config.ts
@@ -1,12 +1,8 @@
 import 'dotenv/config';
-import mysql, { Connection } from 'mysql2';
+import mysql, { Pool } from 'mysql2';
 
-const connection: Connection = mysql.createConnection({
-  host: process.env.DB_HOST, // address of the server
-  port: Number(process.env.DB_PORT), // port of the DB server (mysql), not to be confused with the nodeJS server PORT !
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// créer l'objet pool
+const pool: Pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL || '');
 
-export default connection;
+// exporte l'objet pool pour l'utiliser ailleurs
+export default pool;
